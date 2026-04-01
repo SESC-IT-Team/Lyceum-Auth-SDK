4ba0ca23b1dcef2b30872b9a7e27c5dbf03a22a0
Dunin Dmitry Andreevich <d.dunin1709@gmail.com>
2026-04-01 21:43:05 +0500

    Asyncio bug fixes

--- sesc_auth_sdk/dependencies.py
+++ sesc_auth_sdk/dependencies.py
@@ -1,7 +1,9 @@
 from typing import Annotated, Optional
 
+from _testcapi import awaitType
 from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic_settings.sources.providers import aws
 
 from sesc_auth_sdk.enums.role import Role
 from sesc_auth_sdk.schemas.jwt import JwtPayload
@@ -28,10 +30,10 @@ class LyceumAuth:
     async def verify_authorized(token: str | None = Depends(_get_token)) -> JwtPayload:
         if not token:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-        return jwks_manager.verify_token(token)
+        return await jwks_manager.verify_token(token)
 
 
-    async def __call__(self, token_payload: Annotated[JwtPayload, Depends(verify_authorized)]):
+    async def __call__(self, token_payload: JwtPayload = Depends(verify_authorized)) -> JwtPayload:
         if self.allowed_roles and token_payload.role not in self.allowed_roles:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
@@ -40,10 +42,10 @@ class LyceumAuth:
         return token_payload
 
     async def return_user(self, token: str = Depends(_get_token)) -> UserSchema:
-        self()
-        return RequestsService.get_me(token)
+        await self()
+        return await RequestsService.get_me(token)
 
     @staticmethod
-    def get_current_user(token: str = Depends(_get_token), _ = Depends(verify_authorized)) -> UserSchema:
-        return RequestsService.get_me(token)
+    async def get_current_user(token: str = Depends(_get_token), _ = Depends(verify_authorized)) -> UserSchema:
+        return await RequestsService.get_me(token)
 
--- sesc_auth_sdk/services/jwks_manager.py
+++ sesc_auth_sdk/services/jwks_manager.py
@@ -10,11 +10,11 @@ class JWKSManagerClass:
     def __init__(self):
         self._keys: dict[str, Jwk] = {}
 
-    def get_key(self, kid: str):
+    async def get_key(self, kid: str):
         try:
             return self._keys[kid]
         except KeyError:
-            self.update_keys()
+            await self.update_keys()
             return self._keys.get(kid)
 
     async def update_keys(self):
@@ -23,13 +23,13 @@ class JWKSManagerClass:
         for key in keys:
             self._keys[key.kid] = key
 
-    def verify_token(self, token: str) -> JwtPayload:
+    async def verify_token(self, token: str) -> JwtPayload:
         try:
             unverified_headers = JwtHeaders(**jwt.get_unverified_header(token))
             kid = unverified_headers.kid
             if not kid:
                 raise JWTError("Field kid missed in headers of token")
-            key = self.get_key(kid)
+            key = await self.get_key(kid)
             if not key:
                 raise JWTError("Public key not found in JWKS")
             return JwtPayload(**jwt.decode(token, jwt.PyJWK(key.model_dump()), algorithms=["RS256"], issuer=settings.auth_base_url, options={'verify_iss': True, 'verify_exp': True, "verify_signature": True}))
