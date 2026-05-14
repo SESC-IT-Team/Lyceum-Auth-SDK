d7e54e53e960b8cee8442fc5ad0fc4cb1f2d7edd
Daniil Sochnev <sochnevde09@gmail.com>
2026-05-14 22:02:22 +0500

    bug fix

--- sesc_auth_sdk/config.py
+++ sesc_auth_sdk/config.py
@@ -21,5 +21,5 @@ class AuthSDKConfig(BaseSettings):
     jwks_ttl: int = 900
     use_statics: bool = False
     static_jwt_user: JwtUserSchema = JwtUserSchema(id=uuid4(), roles=[Role.student, Role.admin], permissions=[Permissions.Auth.Users.create, Permissions.Auth.Users.read, Permissions.Auth.Users.update, Permissions.Auth.Users.delete])
-    static_user: UserSchema = UserSchema(id=uuid4(), last_name='Ivanov', first_name='Ivan', middle_name='Ivanovich', roles=[Role.student, Role.admin], gender=Gender.male, class_name='10В', graduation_year=2027, login='IvanovIvan1234567', permissions=[Permissions.Auth.Users.create, Permissions.Auth.Users.read, Permissions.Auth.Users.update, Permissions.Auth.Users.delete], created_at=datetime.now(), updated_at=datetime.now(), full_name='GOL', letter='A', grade=10, birthday=date(2026, 5, 2))
+    static_user: UserSchema = UserSchema(id=uuid4(), last_name='Ivanov', first_name='Ivan', middle_name='Ivanovich', roles=[Role.student, Role.admin], gender=Gender.male, class_name='10В', graduation_year=2027, login='IvanovIvan1234567', permissions=[Permissions.Auth.Users.create, Permissions.Auth.Users.read, Permissions.Auth.Users.update, Permissions.Auth.Users.delete], created_at=datetime.now(), updated_at=datetime.now(), full_name='GOL', letter='A', grade=10, birthday=date(2026, 5, 2), department=None)
 settings = AuthSDKConfig()
