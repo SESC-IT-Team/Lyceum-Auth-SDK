3e5b71570d56314fe0a5561e0b495c3e1dd9ad2d
Sr_lucky_MaN <d.dunin1709@gmail.com>
2026-07-21 11:32:51 +0500

    Renaming

--- sesc_auth_sdk/enums/scope.py
+++ sesc_auth_sdk/enums/scope.py
@@ -10,6 +10,6 @@ class Scope(str, Enum):
     spravki_orders_get = 'spravki:orders:get'
     spravki_orders_get_my = 'spravki:orders:get_my'
     auth_users_create = 'auth:users:create'
-    auth_users_get = 'auth:users:read'
+    auth_users_read = 'auth:users:read'
     auth_users_update = 'auth:users:update'
     auth_users_delete = 'auth:users:delete'
