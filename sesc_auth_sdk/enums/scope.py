6cc7e34dd196d5227565049c8b47123b691cb8d3
Sr_lucky_MaN <d.dunin1709@gmail.com>
2026-08-03 18:10:26 +0500

    Scope to read children added

--- sesc_auth_sdk/enums/scope.py
+++ sesc_auth_sdk/enums/scope.py
@@ -6,9 +6,12 @@ class Scope(str, Enum):
     openid = 'openid'
     email = 'email'
     offline_access = 'offline_access'
+
     spravki_orders_create = 'spravki:orders:create'
     spravki_orders_get = 'spravki:orders:get'
     spravki_orders_get_my = 'spravki:orders:get_my'
+    auth_children_read = 'auth:children:read'
+
     auth_users_create = 'auth:users:create'
     auth_users_read = 'auth:users:read'
     auth_users_update = 'auth:users:update'
