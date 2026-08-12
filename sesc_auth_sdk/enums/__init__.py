53b6edb79542158054e6bde8d19eafd318d062ba
Dunin Dmitry Andreevich <d.dunin1709@gmail.com>
2026-08-13 01:07:56 +0500

    department role -> department position bug fix

--- sesc_auth_sdk/enums/__init__.py
+++ sesc_auth_sdk/enums/__init__.py
@@ -1,8 +1,8 @@
 from sesc_auth_sdk.enums.department import Department
-from sesc_auth_sdk.enums.department_member_position import DepartmentMemberRole
+from sesc_auth_sdk.enums.department_member_position import DepartmentMemberPosition
 from sesc_auth_sdk.enums.gender import Gender
 from sesc_auth_sdk.enums.role import Role
 from sesc_auth_sdk.enums.scope import Scope
 
 
-__all__ = ["Department", "DepartmentMemberRole", "Gender", "Role", "Scope"]
+__all__ = ["Department", "DepartmentMemberPosition", "Gender", "Role", "Scope"]
